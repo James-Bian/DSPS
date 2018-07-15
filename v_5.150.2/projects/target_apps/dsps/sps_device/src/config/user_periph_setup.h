ddd4f7c7aeb0f7b49ec63b92f9aa54ac5d711734
james.bian <bianzhenchu@163.com>
2018-07-15 16:15:35 +0800

    modify sps uart port pin number, avoide confilict with download port p04 and p05

--- v_5.150.2/projects/target_apps/dsps/sps_device/src/config/user_periph_setup.h
+++ v_5.150.2/projects/target_apps/dsps/sps_device/src/config/user_periph_setup.h
@@ -33,7 +33,7 @@
  ****************************************************************************************
  */
 
-#under CFG_UART_HW_FLOW_CTRL
+#undef CFG_UART_HW_FLOW_CTRL
 #undef CFG_UART_SW_FLOW_CTRL
 
 /// Available baud rates 115200, 57600, 38400, 19200, 9600
@@ -47,13 +47,13 @@
 /****************************************************************************************/  
 
     #define GPIO_UART1_TX_PORT   GPIO_PORT_0
-    #define GPIO_UART1_TX_PIN    GPIO_PIN_4
+    #define GPIO_UART1_TX_PIN    GPIO_PIN_1
     #define GPIO_UART1_RX_PORT   GPIO_PORT_0
-    #define GPIO_UART1_RX_PIN    GPIO_PIN_5
+    #define GPIO_UART1_RX_PIN    GPIO_PIN_2
     #define GPIO_UART1_RTS_PORT  GPIO_PORT_0
     #define GPIO_UART1_RTS_PIN   GPIO_PIN_3
     #define GPIO_UART1_CTS_PORT  GPIO_PORT_0
-    #define GPIO_UART1_CTS_PIN   GPIO_PIN_2
+    #define GPIO_UART1_CTS_PIN   GPIO_PIN_4
 
 /****************************************************************************************/ 
 /* UART2 GPIO configuration                                                             */
